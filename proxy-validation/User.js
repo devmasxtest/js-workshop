ae3f87bb870d2454bd83c6cc5e6d2e286f4638c7
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-06-18 00:04:34 +0200

    get and set in es6 class

---
+++ proxy-validation/User.js
@@ -0,0 +1,34 @@
+let _name = "";
+const balances = [];
+
+class User {
+  get name() {
+    return _name;
+  }
+  set name(value) {
+    _name = value.toUpperCase();
+  }
+
+  get balanceHistory() {
+    return balances;
+  }
+
+  get balance() {
+    return balances[0];
+  }
+
+  set balance(value) {
+    balances.unshift(value);
+  }
+}
+
+const user = new User();
+user.name = "Miguel";
+
+console.log(user.name);
+
+user.balance = 12;
+user.balance = 10;
+user.balance = 7;
+console.log(user.balance);
+console.log(user.balanceHistory);
