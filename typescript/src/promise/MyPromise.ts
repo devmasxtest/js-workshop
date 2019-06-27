0eecc6761d801c79d8b0e0ba8f8027a7fba9788f
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-06-27 17:59:43 +0200

    .

--- typescript/src/promise/MyPromise.ts
+++ typescript/src/promise/MyPromise.ts
@@ -1,15 +1,21 @@
 class MyPromise {
   value;
+  error;
+  hasError: boolean;
+
   constructor(handler) {
     handler(this.resolve.bind(this), this.reject.bind(this));
   }
 
   resolve(value) {
     // console.log(value);
+    this.hasError = false;
     this.value = value;
   }
 
-  reject() {}
+  reject(error) {
+    this.error = error;
+  }
 
   then(cb) {
     cb(this.value);
@@ -17,6 +23,10 @@ class MyPromise {
   }
 
   catch(cb) {
+    if (this.hasError) {
+      cb(this.error);
+    }
+
     return this;
   }
 }
