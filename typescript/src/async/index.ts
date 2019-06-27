e5a5189ac1308045239a03dd0c82b2f5712e15d2
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-06-27 18:40:58 +0200

    .

--- typescript/src/async/index.ts
+++ typescript/src/async/index.ts
@@ -8,30 +8,33 @@ const sumAsync = (a, b) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(a + b);
-    }, 5 * 1000);
+    }, 1 * 1000);
   });
 
 (async function() {
-  console.time("start");
-  const r = await Promise.all([
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2),
-    sumAsync(1, 2)
-  ]);
-  // const r = await sumAsync(1, 2);
-  console.timeEnd("start");
+  // console.time("start");
+  // const r = await Promise.all([sumAsync(1, 2), sumAsync(1, 2)]);
 
-  console.log(r);
+  sumAsync(1, 2)
+    .then(total => {
+      console.log("TOTAL 1");
+      // sumAsync(total, 3).then(() => {
+      //   console.log("TOTAL 2");
+      // });
+
+      setTimeout(() => {
+        console.log("TOTAL 2");
+      }, 3 * 1000);
+
+      return sumAsync(total, 4);
+    })
+    .then(total => {
+      console.log("TOTAL 3");
+    });
+  console.log("FINISH");
+
+  // const r = await sumAsync(1, 2);
+  // console.timeEnd("start");
+
+  // console.log(r);
 })();
--- typescript/src/promise/MyPromise.ts
+++ typescript/src/promise/MyPromise.ts
@@ -1,7 +1,10 @@
+import * as fs from "fs";
+
 class MyPromise {
   value;
   error;
   hasError: boolean;
+  finish: boolean;
 
   constructor(handler) {
     handler(this.resolve.bind(this), this.reject.bind(this));
@@ -15,12 +18,11 @@ class MyPromise {
 
   reject(error) {
     this.hasError = true;
-
     this.error = error;
   }
 
   then(cb) {
-    if (!this.hasError) {
+    if (this.finish && !this.hasError) {
       cb(this.value);
     }
     return this;
@@ -35,13 +37,21 @@ class MyPromise {
   }
 }
 
-const mypromise = new MyPromise((resolve, reject) => {
-  reject(3);
-  // resolve(10);
-});
+const myFnc = filePath =>
+  new MyPromise((resolve, reject) => {
+    console.log("filePath", filePath);
+    fs.readFile(filePath, { encoding: "utf8" }, (err, result) => {
+      console.log("READFILE FINISH");
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
 
-mypromise
+myFnc(__dirname + "text.txt")
   .then(result => {
-    console.log(result);
+    console.log("RESOLVE", result);
   })
-  .catch(console.error);
+  .catch(error => console.error("ERROR", error));
